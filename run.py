b109d2549725d4506fa822bc813ac451ab3dcdbf
Amnasaqib <>
2024-01-07 20:58:26 +0000

    Remove errors

--- run.py
+++ run.py
@@ -165,63 +165,63 @@ def get_date(reason):
                print("- Be in the format of dd/mm/yyyy.")
                print("- Contain realistic values for day, month and year.\n")
                print("Please input a valid date.")
-           else:
-                if: reason == "book":
+            else:
+                 if: reason == "book":
                      date_available = bool(get_avail_times(date_input))
                      if date_available is False:
                          print(f"Sorry, {date_input} is unavailable.")
                          print("Please enter a new date.")
                      else:
-                        if CURRENT_DATE > date_fm:
+                         if CURRENT_DATE > date_fm:
                             print("Invalid date input (past date).\n")
                             print("Please enter a present or future date.")
-                        else:
+                         else:
                             break
-                elif reason == "search":
-                    break
+                 elif reason == "search":
+                     break
 
      return date_input
 
 
 def get_time(data):
-    """
-    Provides list options of available times and requests input for desired
+     """
+     Provides list options of available times and requests input for desired
     time. If only one time is available it prompts the user to continue or
-    enter a new date. Requests input until it is valid or returns to menu
-    if 'Exit' is input.
-    """
-    clear_tmnl()
-    times = get_avail_times(data)
+     enter a new date. Requests input until it is valid or returns to menu
+     if 'Exit' is input.
+     """
+     clear_tmnl()
+     times = get_avail_times(data)
      if len(times) == 1:
-        print(f"The only available time on {data} is {times[0]}.\n")
-        print("Press 1 to continue with this time or 2 to enter a new date.")
-        while True:
-            time_ans = input("")
-            if time_ans not in ("1", "2"):
-                print("Invalid input.\n")
-                print("Please choose an option between 1 and 2.")
-            else:
-                break
-        if time_ans == "1":
-            time_input = times[0]
-            return time_input
-        else:
-            collect_details()
+         print(f"The only available time on {data} is {times[0]}.\n")
+         print("Press 1 to continue with this time or 2 to enter a new date.")
+         while True:
+             time_ans = input("")
+             if time_ans not in ("1", "2"):
+                 print("Invalid input.\n")
+                 print("Please choose an option between 1 and 2.")
+             else:
+                 break
+         if time_ans == "1":
+             time_input = times[0]
+             return time_input
+         else:
+             collect_details()
 
-        else:
-        print(f"Below is a list of available times for {data}.\n")
-        time_input = pyip.inputMenu(times,
-                                    prompt="Select a time from the list.\n",
-                                    numbered=True,
-                                    allowRegexes=[("Exit"), ("exit")]
-                                    )
-        if time_input.capitalize() == "Exit":
-            main_menu()
-        else:
-            return time_input
+         else:
+         print(f"Below is a list of available times for {data}.\n")
+         time_input = pyip.inputMenu(times,
+                                     prompt="Select a time from the list.\n",
+                                     numbered=True,
+                                     allowRegexes=[("Exit"), ("exit")]
+                                     )
+         if time_input.capitalize() == "Exit":
+             main_menu()
+         else:
+             return time_input
 
 
-  def get_avail_times(data):
+ def get_avail_times(data):
      """
      Gets return value from get_appts_for_date function for booked times
      and removes them from the appointment times list to create a list of
