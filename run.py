26dbe60e260730a3d9d4079e4f42c3df56e14e28
Amnasaqib <>
2024-01-07 19:18:29 +0000

    Fix errors

--- run.py
+++ run.py
@@ -75,7 +75,7 @@ def main_menu():
             if search_ans not in ("1", "2", "3"):
                print("Invalid input.")
                print("Please choose an option between 1 and 2.")
-           else:
+        else:
                break
 
         if search_ans == ("1"):
